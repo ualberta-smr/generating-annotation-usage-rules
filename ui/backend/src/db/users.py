f0074f7ff24c17b1084f122987cd011aa140ac2c
MensurOwary <mensurowary@gmail.com>
2022-03-28 00:08:02 -0600

    Added the feature to be able to separate different rule datasets for different users (#15)

--- ui/backend/src/db/users.py
+++ ui/backend/src/db/users.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List
 
 from fastapi import Response
 from .models import User
@@ -7,10 +7,18 @@ from sqlalchemy.orm import Session
 
 USER_MAPPING = {
     "mansur": 1,
-    "emily": 2,
+    "ajay": 2,
     "user1": 3,
     "user2": 4,
     "user3": 5,
+}
+
+USER_PACKAGE_MAPPING = {
+    1: {"file": "./data/users/data_mansur.json", "package_id": 1},
+    2: {"file": "./data/users/data_ajay.json", "package_id": 2},
+    3: {"file": "./data/users/data_user1.json", "package_id": 3},
+    4: {"file": "./data/users/data_user2.json", "package_id": 4},
+    5: {"file": "./data/users/data_user3.json", "package_id": 5}
 }
 
 class UserOperationsHandler:
@@ -30,4 +38,11 @@ class UserOperationsHandler:
             response.status_code = 200
         else:
             response.status_code = 404
-        
+    
+    @staticmethod
+    def getUserPackageMapping() -> Dict[int, Dict]:
+        return USER_PACKAGE_MAPPING
+
+    @staticmethod
+    def getPackageIdForUser(user_id: int) -> int:
+        return USER_PACKAGE_MAPPING[user_id]["package_id"]
