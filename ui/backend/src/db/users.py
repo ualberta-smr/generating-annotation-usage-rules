8f2e5a507b6000c7997e0b8c49c6a9ea4750ae60
MensurOwary <mensurowary@gmail.com>
2022-04-30 02:11:35 -0600

    Rule partitioning is done

--- ui/backend/src/db/users.py
+++ ui/backend/src/db/users.py
@@ -7,11 +7,19 @@ from sqlalchemy.orm import Session
 
 # username => user_id
 USER_MAPPING = {
+    # dummy data
     "mansur": 1,
     "ajay": 2,
     "user1": 3,
     "user2": 4,
     "user3": 5,
+    # experiment subjects
+    "P5": 6,
+    "P1": 7,
+    "P3": 8,
+    "P6": 9,
+    "P2": 10,
+    "P4": 11
 }
 
 # user_id => package_info
@@ -20,7 +28,15 @@ USER_PACKAGE_MAPPING = {
     2: {"file": "./data/target_set.json", "package_id": 2},
     3: {"file": "./data/users/data_user1.json", "package_id": 3},
     4: {"file": "./data/users/data_user2.json", "package_id": 4},
-    5: {"file": "./data/target_set.json", "package_id": 5}
+    5: {"file": "./data/target_set.json", "package_id": 5},
+
+
+    6: {"file": "./data/experiment_data/P5.json", "package_id": 6},
+    7: {"file": "./data/experiment_data/P1.json", "package_id": 7},
+    8: {"file": "./data/experiment_data/P3.json", "package_id": 8},
+    9: {"file": "./data/experiment_data/P6.json", "package_id": 9},
+    10: {"file": "./data/experiment_data/P2.json", "package_id": 10},
+    11: {"file": "./data/experiment_data/P4.json", "package_id": 11},
 }
 
 class UserOperationsHandler:
