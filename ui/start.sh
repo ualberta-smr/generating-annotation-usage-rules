4a7c56b0abbafa8242cecb18b25822fb0d9409d9
MensurOwary <mensurowary@gmail.com>
2022-04-30 18:14:47 -0600

    Minor changes to the start script

--- ui/start.sh
+++ ui/start.sh
@@ -1,10 +1,11 @@
 # this script basically stops everything and restarts them but re-builds 1 service of choice in the meantime
 # run the start.sh with a parameter or no param (default will be backend)
-docker-compose down
 # start.sh localhost
 
 host="http://${1:-localhost}"
 backend="$host:5000"
 tutorial="$host:8000/README.md"
 
-RVT_BACKEND="${backend}" RVT_TUTORIAL="${tutorial}"; docker-compose up --build -d
+docker-compose down
+
+RVT_BACKEND="${backend}" RVT_TUTORIAL="${tutorial}" docker-compose up --build -d
