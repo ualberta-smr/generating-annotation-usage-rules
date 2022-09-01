3d13c59cf763a72d23912f385230669b57b32e0e
MensurOwary <mensurowary@gmail.com>
2022-08-31 18:25:23 -0600

    Clean up the test

--- violation-detector/violation-detection/src/test/java/ca/ualberta/smr/testing/DummyClassTest.java
+++ violation-detector/violation-detection/src/test/java/ca/ualberta/smr/testing/DummyClassTest.java
@@ -2,9 +2,7 @@ package ca.ualberta.smr.testing;
 
 import ca.ualberta.smr.model.StaticAnalysisRule;
 import ca.ualberta.smr.model.violationreport.ViolationCombination;
-import ca.ualberta.smr.parsing.rules.Rule;
-import ca.ualberta.smr.parsing.rules.RuleParser;
-import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
@@ -19,7 +17,7 @@ public class DummyClassTest {
     final DefaultViolationDetector defaultDetector = new DefaultViolationDetector("src/test/data/jarfiles");
 
     @Test
-    @Ignore
+    @Disabled
     public void test() {
         final Map<StaticAnalysisRule, Collection<ViolationCombination>> violations = defaultDetector.analyze(
                 ""
